55621e47bcea1ccb170076e3f2d23d9524b5fd43
Deepak <deepak.jpjl@gmail.com>
2019-01-02 19:27:23 +0000

    Signup + Flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def signup
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new permit_User_create
+    if @user.save
+      flash[:success] = "Welcome #{@user.name}, you have successfully signed up!"
+      redirect_to user_url(@user.id)
+    else
+      render 'signup'
+    end
+  end
+  
+  private
+    def permit_User_create
+      params.require(:user).permit(:name,:email,:password)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'static_pages/about', to: 'static_pages#about'
   
   get 'static_pages/contact', to: 'static_pages#contact'
+  
+  resources :users
 
   #root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
